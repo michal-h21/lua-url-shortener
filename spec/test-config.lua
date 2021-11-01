ebfd7d87129a94bf79a8bf88f1a15ca6d0b4d6ab
Michal Hoftich <michal.h21@gmail.com>
2021-11-01 12:02:47 +0100

    config works

--- spec/test-config.lua
+++ spec/test-config.lua
@@ -1,7 +1,15 @@
 local config = require "lua-url-shortener.config"
 
-local test_config_filename = "spec/config-sample.lua"
-local test, msg = config.load_config(test_config_filename)
 
-print(config.file_exists(test_config_filename))
+describe("Test config file", function()
+  local test_config_filename = "spec/config-sample.lua"
+  it("should find config file", function()
+    assert.truthy(config.file_exists(test_config_filename))
+  end)
+  local test, msg = config.load_config(test_config_filename)
+  it("should return table with config values", function()
+    assert.same(test.a, 10)
+    assert.same(test.b, 20)
+  end)
+end)
 
