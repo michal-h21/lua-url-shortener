e010ca4ddf6083406674f2d1f2e0003b2b2e10bb
Michal Hoftich <michal.h21@gmail.com>
2021-11-06 00:14:21 +0100

    added info and warning functions

--- src/lua-url-shortener/log.lua
+++ src/lua-url-shortener/log.lua
@@ -4,4 +4,12 @@ function M.error(msg)
   print("[Error] " .. msg)
 end
 
+function M.warning(msg)
+  print("[Warning] " .. msg)
+end
+
+function M.info(msg)
+  print("[Info] " .. msg)
+end
+
 return M
